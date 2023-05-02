879b046a8a07c81bce353a38413301615ee5ce43
kaseyli <kaseyli@tencent.com>
2023-05-02 17:20:15 +0800

    添加readme

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 拉取当前仓库最新配置
+git pull origin master
+
+# 创建 .CSGuide 目录 用来初始化内容仓库
+mkdir -p docs/.CSGuide
+
+# 进入 docs 目录
+cd docs/.CSGuide
+
+git init
+
+# Check if 'origin' remote exists
+if ! git remote | grep -q "^origin$"; then
+  # If 'origin' does not exist, add it
+  git remote add origin git@github.com:imarvinle/CSGuide.git
+fi
+
+# 拉取最新内容文件
+git pull origin master
+
+# 拷贝内容文件到上一级目录
+
+/bin/cp -rf ./* ../
+
+# 回到上一级目录
+cd -
+
+# 编译构建
+yarn install
+
+yarn docs:build
+
+echo "构建成功，✿✿ヽ(°▽°)ノ✿"
+
+# 拷贝文件到 nginx 映射目录
+
+mkdir -p ~/csguide
+/bin/cp -rf docs/.vuepress/dist/*  ~/csguide/
