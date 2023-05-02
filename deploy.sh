ca3db33bfdc71a5fdd5afae7429d595292f5d8b6
kaseyli <kaseyli@tencent.com>
2023-05-02 17:39:22 +0800

    修改 deploy

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
+# 让脚本抛出遇到的错误
 set -e
 
 # 拉取当前仓库最新配置
 git pull origin master
 
-# 创建 .CSGuide 目录 用来初始化内容仓库
-mkdir -p docs/.CSGuide
+# 创建 docs 目录 用来初始化内容仓库
+mkdir -p docs
 
 # 进入 docs 目录
-cd docs/.CSGuide
+cd docs
 
 git init
 
 # Check if 'origin' remote exists
 if ! git remote | grep -q "^origin$"; then
   # If 'origin' does not exist, add it
-  git remote add origin git@github.com:imarvinle/CSGuide.git
+  git remote add origin git@github.com:imarvinle/gzh_csguide.git
 fi
 
 # 拉取最新内容文件
 git pull origin master
 
-# 拷贝内容文件到上一级目录
+# # 拷贝内容文件到上一级目录
 
-/bin/cp -rf ./* ../
+# /bin/cp -rf ./* ../
 
 # 回到上一级目录
 cd -
